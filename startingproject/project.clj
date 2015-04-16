a61a93ee5347c1778be71a26279719ee2929b05c
jelicatus <jecka_cutie@ymail.com>
2015-04-16 22:49:38 +0200

    First commit. Trying out clojure web dev and using Best Buy API to get reviews.

---
+++ startingproject/project.clj
@@ -0,0 +1,17 @@
+(defproject startingproject "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring "1.0.1"] ;definsemo zavisnost, prvo za server
+                 [compojure "1.0.1"] ;za rute
+                 [enlive "1.0.0"] ;za formatiranje html-a
+                 [org.clojure/data.json "0.2.6"] ;za parsiranje json-a
+                 [clj-http "1.1.0"] ;za skidanje jsona
+                 [local/jml "2.8"]
+                 [local/LIBLINEAR "1.0"]
+                 [local/commons-math "2.2"]
+                 [local/jmathplot "1.0"]]
+   :repositories {"startingproject" "file:repo"}
+  )
---
+++ startingproject/src/startingproject/app.clj
@@ -0,0 +1,93 @@
+(ns startingproject.app 
+  (use (compojure handler [core :only (GET POST defroutes)])) ;handler za ovo je defroutes 
+  (require [ring.adapter.jetty :as jetty] ;jetty je server
+           [ring.util.response :as response] ;util je za response
+           [net.cgrand.enlive-html :as en] ;za formatiranje - pritupanje html elemntima, aributima
+           [compojure.route] ;za definisanje ruta
+           [clojure.data.json :as json] ;za parsiranje json-a
+           [clj-http.client :as client] ;za skidanje jsona
+           [clojure.string :as string])
+;  (import jml.clustering.NMF)
+  )
+
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+(def a 10)
+
+(def counter (atom 10000))
+
+(def mapasajtova (atom {}))
+
+(en/deftemplate homepage
+  (en/xml-resource "sajtovi.html")
+  [request]
+  [:#listing :li]
+  (en/clone-for [[id url] @mapasajtova] [:a] (en/content (str id  " " url)) 
+                                        [:a] (en/set-attr :href (str \/ id))
+  ))
+
+(defn probanje
+  [request]
+  (response/redirect "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment")
+  )
+
+(defn redirect 
+  [id]
+  (response/redirect (@mapasajtova id)) ;@ posto je atom
+  )
+
+(defn napunimapu
+  [url]
+  (let [id (swap! counter inc) id (java.lang.Long/toString id 36)]
+  (swap! mapasajtova assoc id url)
+  )  
+  )
+
+;server se startuje na sledeci nacin:
+;(jetty/run-jetty #'app {:port 8080 :join? false})
+
+;defnisanje ruta
+(defroutes app*
+  (GET "/" request (homepage request))
+  (GET "/proba" request (probanje request))
+  (GET "/:id" [id] (redirect id))
+  (POST "/napunimapu" request (napunimapu (-> request :params :url)) (response/redirect "/") )
+  )
+
+(client/put "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
+  {:form-params "body"
+   :content-type :json
+   :throw-exceptions false
+   :as :json})
+
+(def app (compojure.handler/site app*)) ;middleware xD
+
+; ((json/read-str dzejson :key-fn keyword) :a) --->prvo napravi mapu od dzejsona pa onda se normalno pristupa elementima mape :D
+
+; (((client/get "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
+;     {:form-params "body"
+;      :content-type :json
+;      :as :json} :headers) :body) :reviews) ---->dobijanje review-ova
+
+
+;******************************************************************
+
+;(def reviews-body (((client/get "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
+;     {:form-params "body"
+;      :content-type :json
+;      :as :json} :headers) :body) :reviews))
+
+;(def sequence-of-reviews (map string/lower-case (for [i (range(count reviews-body))] ((reviews-body i) :comment))))
+
+(let [reviews-body (((client/get "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
+        {:form-params "body"
+         :content-type :json
+         :as :json} :headers) :body) :reviews)] 
+     (def sequence-of-reviews (map string/lower-case (for [i (range(count reviews-body))] ((reviews-body i) :comment)))) )
+
+
