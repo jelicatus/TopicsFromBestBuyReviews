c60826e6200118ba16a90d8c89e8ed339493feb5
jelicatus <jecka_cutie@ymail.com>
2015-05-23 16:13:36 +0200

    Started working on web page

--- startingproject/project.clj
+++ startingproject/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring "1.0.1"] ;definsemo zavisnost, prvo za server
+               ;  [ring "1.0.1"] ;definsemo zavisnost, prvo za server
                  [compojure "1.0.1"] ;za rute
                  [enlive "1.0.0"] ;za formatiranje html-a
                  [org.clojure/data.json "0.2.6"] ;za parsiranje json-a
@@ -12,6 +12,7 @@
                  [local/jml "2.8"]
                  [local/LIBLINEAR "1.0"]
                  [local/commons-math "2.2"]
-                 [local/jmathplot "1.0"]]
+                 [local/jmathplot "1.0"]
+                 [http-kit "2.1.16"]]
    :repositories {"startingproject" "file:repo"}
   )
--- startingproject/src/startingproject/app.clj
+++ startingproject/src/startingproject/app.clj
@@ -1,6 +1,7 @@
 (ns startingproject.app 
   (use (compojure handler [core :only (GET POST defroutes)])) ;handler za ovo je defroutes 
-  (require [ring.adapter.jetty :as jetty] ;jetty je server
+  (require ;[ring.adapter.jetty :as jetty] ;jetty je server
+           [org.httpkit.server :as httpserver]
            [ring.util.response :as response] ;util je za response
            [net.cgrand.enlive-html :as en] ;za formatiranje - pritupanje html elemntima, aributima
            [compojure.route] ;za definisanje ruta
@@ -10,79 +11,111 @@
 ;  (import jml.clustering.NMF)
   )
 
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;(httpserver/run-server #'app {:port 8080 :ip "localhost" :join? false})
 
 
-(def a 10)
-
-(def counter (atom 10000))
+(def products (atom ""))
 
 (def mapasajtova (atom {}))
 
-(en/deftemplate homepage
-  (en/xml-resource "sajtovi.html")
-  [request]
-  [:#listing :li]
-  (en/clone-for [[id url] @mapasajtova] [:a] (en/content (str id  " " url)) 
-                                        [:a] (en/set-attr :href (str \/ id))
-  ))
+;(en/deftemplate homepage
+;  (en/xml-resource "index.html")
+;  [request]
+;  [:#listing :li]
+;  (en/clone-for [[id url] @mapasajtova] [:a] (en/content (str id  " " url)) 
+;                                        [:a] (en/set-attr :href (str \/ id))
+;  ))
 
-(defn probanje
+(en/deftemplate probanje 
+  (en/xml-resource "index.html")
   [request]
-  (response/redirect "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment")
+;  (response/redirect "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment")
   )
+
+(en/deftemplate fill-products 
+  (en/xml-resource "index.html")
+  [productss]
+  [:#products] 
+  (en/clone-for[product  productss] 
+     [:h2.name] (en/content (str (product :name)))
+     [:img.product-image] (en/set-attr :src (str (product :image)))
+     [:p.manufacturer] (en/content (str (product :manufacturer)))
+     [:p.prices] (en/content (str "regularPrice " (product :regularPrice) " "))
+     [:p.long-description] (en/content (str (product :longDescription) " "))                        
+     [:h1.url] (en/content (str (product :url)))
+     )
+                             )
+  
 
 (defn redirect 
   [id]
   (response/redirect (@mapasajtova id)) ;@ posto je atom
   )
 
-(defn napunimapu
-  [url]
-  (let [id (swap! counter inc) id (java.lang.Long/toString id 36)]
-  (swap! mapasajtova assoc id url)
-  )  
-  )
+;(defn napunimapu
+ ; [url]
+  ;(let [id (swap! counter inc) id (java.lang.Long/toString id 36)]
+  ;(swap! mapasajtova assoc id url)
+  ;)  
+  ;)
+
+(defn searchforproducts [product]
+   (reset! products
+    {:data   (((client/get (str "http://api.remix.bestbuy.com/v1/products(name=" product "*&categoryPath.name=%22Cell%20Phones%22)?show=name,image,manufacturer,regularPrice,onSale,percentSavings,salePrice,longDescription&pageSize=30&page=30&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&format=json")
+           {:form-params "body"
+            :content-type :json
+            :as :json}
+            ) :body) :products) } )
+    ;     ) 
+;@products   
+ )
+
+(defn malaproba [request] (probanje request))
 
 ;server se startuje na sledeci nacin:
 ;(jetty/run-jetty #'app {:port 8080 :join? false})
 
 ;defnisanje ruta
 (defroutes app*
-  (GET "/" request (homepage request))
+  ;(GET "/" request (homepage request))
+  (GET "/" request (probanje request))
   (GET "/proba" request (probanje request))
   (GET "/:id" [id] (redirect id))
-  (POST "/napunimapu" request (napunimapu (-> request :params :url)) (response/redirect "/") )
+  (POST "/searchforproducts" [product]
+       
+          
+        ;   (str product) (:product (:params product))
+        (searchforproducts  product)
+           (fill-products (into () (:data @products)))
+       ;)
+          
+         )
+;  (POST "/napunimapu" request (napunimapu (-> request :params :url)) (response/redirect "/") )
   )
-
-(client/put "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
-  {:form-params "body"
-   :content-type :json
-   :throw-exceptions false
-   :as :json})
 
 (def app (compojure.handler/site app*)) ;middleware xD
 
-; ((json/read-str dzejson :key-fn keyword) :a) --->prvo napravi mapu od dzejsona pa onda se normalno pristupa elementima mape :D
 
-; (((client/get "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
-;     {:form-params "body"
-;      :content-type :json
-;      :as :json} :headers) :body) :reviews) ---->dobijanje review-ova
+(httpserver/run-server #'app {:port 8080 :ip "localhost" :join? false})
+
+;(client/put "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
+;  {:form-params "body"
+;   :content-type :json
+;   :throw-exceptions false
+;   :as :json})
 
 
-;******************************************************************
+(((client/get "https://api.remix.bestbuy.com/v1/products((categoryPath.id=pcmcat241600050001))?apiKey=5qxg5sxjbbxa9maxpfrqvqjw&sort=regularPrice.asc&show=sku,color,customerReviewAverage,customerReviewCount,description,details.name,details.value,image,inStoreAvailability,inStoreAvailabilityText,longDescription,manufacturer,mobileUrl,name,onSale,percentSavings,regularPrice,salePrice,shortDescription,thumbnailImage,url&pageSize=10&format=json"
+           {:form-params "body"
+            :content-type :json
+            :as :json}
+            ) :body) :products) ;getting the products
 
-;(def reviews-body (((client/get "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
-;     {:form-params "body"
-;      :content-type :json
-;      :as :json} :headers) :body) :reviews))
-
-;(def sequence-of-reviews (map string/lower-case (for [i (range(count reviews-body))] ((reviews-body i) :comment))))
+(((client/get "http://api.remix.bestbuy.com/v1/products(name=Samsung%20Galaxy*&categoryPath.name=%22Cell%20Phones%22)?show=name,image,subclass,department,class,categoryPath.id,categoryPath.name,manufacturer,regularPrice,onSale,percentSavings,salePrice,longDescription&pageSize=30&page=30&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&format=json"
+           {:form-params "body"
+            :content-type :json
+            :as :json}
+            ) :body) :products) ;drugi nacin
 
 (let [reviews-body (((client/get "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
         {:form-params "body"
@@ -116,20 +149,6 @@
 
 (def tokenized-reviews (distinct (for [ i (range (count pre-tokens))] 
                                     (string/split (nth pre-tokens i) #" ")))) 
-
-;(def w-matrix (for [i (range (count pre-tokens) )] 
-;                  (for [j (range (count tokens))] (if (.contains (nth pre-tokens i) (nth tokens j)) 1 0)))) ;defining the W matrix
-
-;(def w-matrix-alternative (for [i (range (count tokenized-reviews) )] 
-;                  (for [j (range (count tokens))] (if (= (nth tokenized-reviews i) (nth tokens j)) 1 0)))) 
-
-;(def sequence-of-frequencies (apply  map + w-matrix)) ;defining matrix of frequencies
-
-;(def proba (for [i (range (count tokenized-reviews) )] 
-;                     (for [j (range (count tokens))] 
-;      					 (if (.contains (nth tokenized-reviews i) (nth tokens j)) 1 0)  ))) ;
-
-
 
 (def frequencies-of-words-reviews (map frequencies tokenized-reviews)) 
 
@@ -173,9 +192,11 @@
 
 (def topics-with-all-tokens (map #(zipmap tokens %) t-matrix ))
 
-(def topics-with-sorted-weight-of-tokens ( map sort-by second topics-with-all-tokens))
+;(def topics-with-sorted-weight-of-tokens ( map sort-by second topics-with-all-tokens)) ;nesto ne radi odjednom
 
-(def 15-words-for-topics (map #(take-last 15 %) topics-with-sorted-weight-of-tokens))
+(def topics-with-sorted-weight-of-tokens (for [i (range (count topics-with-all-tokens))](sort-by val > (nth topics-with-all-tokens i))))
+
+(def words-for-topics (map #(take 15 %) topics-with-sorted-weight-of-tokens)) 
 
 
 
