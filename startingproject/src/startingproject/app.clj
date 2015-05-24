083b06921961726ba344653f9b2e1bcfaca18f32
jelicatus <jecka_cutie@ymail.com>
2015-05-24 22:46:31 +0200

    Creating get review function

--- startingproject/src/startingproject/app.clj
+++ startingproject/src/startingproject/app.clj
@@ -40,11 +40,16 @@
      [:h2.name] (en/content (str (product :name)))
      [:img.product-image] (en/set-attr :src (str (product :image)))
      [:p.manufacturer] (en/content (str (product :manufacturer)))
-     [:p.prices] (en/content (str "regularPrice " (product :regularPrice) " "))
+     [:p.regularPrice] (en/content (str "regularPrice " (product :regularPrice) " "))
+     [:p.onSale] (en/content (str "onSale " (product :onSale) " "))
+     [:p.percentSavings] (en/content (str "percentSavingse " (product :percentSavings) " "))
+     [:p.salePrice] (en/content (str "salePrice " (product :salePrice) " "))
      [:p.long-description] (en/content (str (product :longDescription) " "))                        
-     [:h1.url] (en/content (str (product :url)))
+     [:a.url] (en/set-attr :href (str (product :url)))
+     [:input.sku] (en/set-attr :name (str (product :sku)))
+     
      )
-                             )
+ )
   
 
 (defn redirect 
@@ -61,7 +66,7 @@
 
 (defn searchforproducts [product]
    (reset! products
-    {:data   (((client/get (str "http://api.remix.bestbuy.com/v1/products(name=" product "*&categoryPath.name=%22Cell%20Phones%22)?show=name,image,manufacturer,regularPrice,onSale,percentSavings,salePrice,longDescription&pageSize=30&page=30&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&format=json")
+    {:data   (((client/get (str "http://api.remix.bestbuy.com/v1/products(name=" product "*&categoryPath.name=%22Cell%20Phones%22)?show=name,sku,url,image,manufacturer,regularPrice,onSale,percentSavings,salePrice,longDescription&pageSize=30&page=30&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&format=json")
            {:form-params "body"
             :content-type :json
             :as :json}
@@ -70,33 +75,16 @@
 ;@products   
  )
 
+
+
 (defn malaproba [request] (probanje request))
 
 ;server se startuje na sledeci nacin:
 ;(jetty/run-jetty #'app {:port 8080 :join? false})
 
-;defnisanje ruta
-(defroutes app*
-  ;(GET "/" request (homepage request))
-  (GET "/" request (probanje request))
-  (GET "/proba" request (probanje request))
-  (GET "/:id" [id] (redirect id))
-  (POST "/searchforproducts" [product]
-       
-          
-        ;   (str product) (:product (:params product))
-        (searchforproducts  product)
-           (fill-products (into () (:data @products)))
-       ;)
-          
-         )
-;  (POST "/napunimapu" request (napunimapu (-> request :params :url)) (response/redirect "/") )
-  )
-
-(def app (compojure.handler/site app*)) ;middleware xD
 
 
-(httpserver/run-server #'app {:port 8080 :ip "localhost" :join? false})
+
 
 ;(client/put "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
 ;  {:form-params "body"
@@ -117,18 +105,27 @@
             :as :json}
             ) :body) :products) ;drugi nacin
 
-(let [reviews-body (((client/get "http://api.remix.bestbuy.com/v1/reviews(sku=1780275)?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment"
-        {:form-params "body"
-         :content-type :json
-         :as :json} :headers) :body) :reviews)] 
-     (def sequence-of-reviews (map string/lower-case (for [i (range(count reviews-body))] ((reviews-body i) :comment)))) )
-
-(def punctuation-marks ["." "!" ":" ","]) ;maybe this doesn't need to be kept
-
 (def forbidden-words [":)" "the" "a" "an" "to" "that" "was" "is" "will" "on" "u" "you" "this"  
                             "can" "could" "my" "his" "has" "from" "each" "of" "one" "our" "we"
                             "he" "and" "for" "us" "her" "\"FaceTime\"" "it" "but" "when" "its" "cery" "don't"
-                             "with" "it" "she" "i'm" "in" "if" "i" "no"]) ;define more words later
+                             "with" "it" "she" "i'm" "in" "if" "i" "no" "yet" "at"]) ;define more words later
+
+(def punctuation-marks ["." "!" ":" "," "/"])
+
+(defn tf-idf [tf_ij N df_i] (* tf_ij (java.lang.Math/log (/ N df_i)))) ;function for getting w_ij 
+
+(def nmfopt (jml.options.NMFOptions.))
+(set! (.epsilon nmfopt) (- java.lang.Math/E 5) )
+(set! (.calc_OV nmfopt) false)
+(set! (.verbose nmfopt) true)
+(set! (.maxIter nmfopt) 50)
+(set! (.nClus nmfopt) 2)
+		
+(def NMFclustering (jml.clustering.NMF. nmfopt))
+
+(defn feed-data [nmfclustering data] (. nmfclustering (feedData data)))
+(defn set-clustering [nmfclustering param] (. nmfclustering (clustering param)))
+
 
 (defn remove-words-from-sentence
   [sentence words]
@@ -136,6 +133,47 @@
                      (string/join "|")
                      (format "(%s)\\s*"))]
     (.trim (.replaceAll sentence pattern "")))) ;function for removing forbidden words
+
+
+(defn get-reviews [sku] 
+  (let [reviews-body (((client/get (str "http://api.remix.bestbuy.com/v1/reviews(sku=" sku ")?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment")
+        {:form-params "body"
+         :content-type :json
+         :as :json} :headers) :body) :reviews)] 
+     (def sequence-of-reviews (map string/lower-case (map :comment reviews-body))) ))
+
+(defn get-reviews1 [sku] 
+  (let [reviews-body (((client/get (str "http://api.remix.bestbuy.com/v1/reviews(sku=" sku ")?format=json&apiKey=5qxg5sxjbbxa9maxpfrqvqjw&show=comment")
+        {:form-params "body"
+         :content-type :json
+         :as :json} :headers) :body) :reviews),
+        sequence-of-reviews (map string/lower-case (map :comment reviews-body)),
+        pre-pre-tokens (map #(remove-words-from-sentence % forbidden-words) sequence-of-reviews),
+        pre-tokens (for [s pre-pre-tokens] (-> s ((apply comp (for [s punctuation-marks] #(.replace %1 s "")))))),
+        tokens (distinct (flatten (map #(string/split % #" ") pre-tokens))),
+        tokenized-reviews (distinct (map #(string/split % #" ") pre-tokens)),
+        frequencies-of-words-reviews (map frequencies tokenized-reviews),
+        frequencies-matrix (for [i (range (count frequencies-of-words-reviews))] (for [j (range (count tokens))] 
+         (if (= ((nth frequencies-of-words-reviews i) (nth tokens j)) nil) 0 ((nth frequencies-of-words-reviews i) (nth tokens j))))),
+        sequence-of-frequencies (apply  map + (for [i (range (count frequencies-of-words-reviews))] (for [j (range (count tokens))] 
+         (if (= ((nth frequencies-of-words-reviews i) (nth tokens j)) nil) 0  1)))),
+        number-of-reviews (count sequence-of-reviews),
+        w-matrix (for [i (range (count frequencies-matrix))] (for [j (range (count sequence-of-frequencies))] 
+         (tf-idf (nth (nth frequencies-matrix i) j) number-of-reviews (nth sequence-of-frequencies j)))),
+        data (into-array (map double-array w-matrix)),
+        step1NMF (feed-data nmfclustering data),
+        step2NMF (set-clustering nmfclustering nil),
+        matrix  (. nmfclustering (getIndicatorMatrix)),
+        t-matrix (apply mapv vector (mapv #(vec (.getRow matrix %)) (range (.getRowDimension matrix)))),
+        topics-with-all-tokens (map #(zipmap tokens %) t-matrix ),
+        topics-with-sorted-weight-of-tokens (for [i (range (count topics-with-all-tokens))](sort-by val > (nth topics-with-all-tokens i)))        
+        ] 
+     (def words-for-topics (map #(take 15 %) topics-with-sorted-weight-of-tokens)) ))
+
+
+(defn findouttopics [sku]
+  (get-reviews sku)
+  )
 
 (def pre-pre-tokens (for [j (range (count sequence-of-reviews))] 
                       (remove-words-from-sentence (nth sequence-of-reviews j) forbidden-words))) ;removing forbidden words
@@ -156,17 +194,17 @@
     (if (= ((nth frequencies-of-words-reviews i) (nth tokens j)) nil) 0 ((nth frequencies-of-words-reviews i) (nth tokens j)))))) ;matrix where element ij represents occurences of token j in review i
 
 
-(def sequence-of-frequencies (apply  map + (for [i (range (count frequencies-of-words-reviews))] (for [j (range (count tokens))] 
+(def sequence-of-frequencies (apply map + (for [i (range (count frequencies-of-words-reviews))] (for [j (range (count tokens))] 
     (if (= ((nth frequencies-of-words-reviews i) (nth tokens j)) nil) 0  1))))) ;sequence where element i represents the number of reviews that contains token i
 
-(defn tf-idf [tf_ij N df_i] (* tf_ij (java.lang.Math/log (/ N df_i)))) ;function for getting w_ij 
+
 
 (def number-of-reviews (count sequence-of-reviews) )
 
 (def w-matrix (for [i (range (count frequencies-matrix))] (for [j (range (count sequence-of-frequencies))] 
     (tf-idf (nth (nth frequencies-matrix i) j) number-of-reviews (nth sequence-of-frequencies j)))))
 
-( def nmfopt (jml.options.NMFOptions.))
+(def nmfopt (jml.options.NMFOptions.))
         (set! (.epsilon nmfopt) (- java.lang.Math/E 5) )
         (set! (.calc_OV nmfopt) false)
         (set! (.verbose nmfopt) true)
@@ -176,6 +214,8 @@
 (def NMFclustering (jml.clustering.NMF. nmfopt))
 
 (def data (into-array (map double-array w-matrix))) 
+
+
  
 (def nmfclustering (jml.clustering.NMF. nmfopt))
 (. nmfclustering (feedData data))
@@ -200,5 +240,30 @@
 
 
 
+(defn top-level-funtion [sku]
+  (let [x (get-reviews sku),
+        y (map fj3 x)]
+    (rep y))
+  )
+
+;defnisanje ruta
+(defroutes app*
+  ;(GET "/" request (homepage request))
+  (GET "/" request (probanje request))
+  (GET "/proba" request (probanje request))
+  (GET "/:id" [id] (redirect id))
+  (POST "/searchforproducts" [product]       
+        (searchforproducts  product)
+           (fill-products (into () (:data @products)))
+       )
+  (POST "/findouttopics" [sku]       
+        (findouttopics sku)
+       )
+;  (POST "/napunimapu" request (napunimapu (-> request :params :url)) (response/redirect "/") )
+  )
 
 
+(def app (compojure.handler/site app*)) ;middleware xD
+
+
+(httpserver/run-server #'app {:port 8080 :ip "localhost" :join? false})
